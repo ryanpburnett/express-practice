4f736e552fe5a76c9149deee7900f8c4197f748d
Ryan <ryanpburnett@yahoo.com>
2021-09-16 14:33:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const app = express()
+const PORT = 412
+
+// middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+let animals = [
+    {
+        animalName: "Lola",
+        species: "Dog",
+        numbers: [1, 7, 3],
+        route: 'lola'
+    },
+    {
+        animalName: "Abby",
+        species: "Dog",
+        numbers: [4, 5, 6],
+        route: 'abby'
+    },
+    {
+        animalName: "Pancakes",
+        species: "Cat",
+        numbers: [7, 8, 9],
+        route: 'pancakes'
+    },
+    {
+        animalName: "Elizardbeth",
+        species: "Unknown",
+        numbers: [1, 10, 8],
+        route: 'elizardbeth'
+    }
+]
+
+// home route
+app.get('/', (req, res) => {
+    res.send("ya site's workin' there, fella")
+})
+
+// ummagumma
+app.get('/ummagumma', (req, res) => {
+    res.send(`<img src='https://images-na.ssl-images-amazon.com/images/I/81-Joj09kIL._SL1300_.jpg'></img>`)
+})
+
+// :routeName is our route parameter
+app.get('/api/animals/:routeName', (req, res) => {
+    // accessing :routeName
+    const targetAnimal = req.params.routeName
+
+    // looping throught animals object to find animal with targetAnimal routeName
+    const animal = animals.find(animal => {
+        return animal.route === targetAnimal
+    })
+
+    //sending data to browser
+    res.send(
+        `${animal.animalName} is barking`
+    )
+
+    res.end()
+})
+
+// API routes
+app.get('/api/animals', (req, res) => {
+    res.json(animals)
+})
+
+app.post('/api/animals/add', (req, res) => {
+    console.log(req.body)
+    res.end()
+})
+
+// Get the dang server to listen
+app.listen(PORT, () => {
+    console.log(`ya server's listening fool - check it on http://localhost:${PORT}`)
+})
+
+
